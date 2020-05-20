0b4cc0c014f3debde56dbae7ddbf82fbb584c877
Aleks <alekey.pakriev@gmail.com>
2020-05-20 23:20:24 +0500

    Написал функцию подсчета, расходов

---
+++ script.js
@@ -0,0 +1,56 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+const dummyTransactions = [
+    {id: 1, text: 'Цветы', amount: -20},
+    {id: 1, text: 'Зарплата', amount: 300},
+    {id: 1, text: 'Книга', amount: -10},
+    {id: 1, text: 'Камера', amount: 150}
+];
+
+let transactions = dummyTransactions;
+
+function addTransactionDOM(transaction) {
+    const sign = transaction.amount < 0 ? '-' : '+';
+    const item = document.createElement('li');
+
+    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+    item.innerHTML = `${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span><button class="delete-btn">x</button>`;
+
+    list.appendChild(item);
+}
+
+
+
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount);
+    const total = amounts.reduce((acc, item) => acc += item, 0).toFixed(2);
+
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => acc += item, 0)
+        .toFixed(2);
+
+    const expense = amounts
+        .filter(item => item < 0)
+        .reduce((acc, item) => acc += item, 0)
+        .toFixed(2) * -1;
+
+    balance.innerText = `$${total}`;
+    money_plus.innerText = `$${income}`;
+    money_minus.innerText = `$${expense}`;
+}
+
+function init() {
+    list.innerHTML = '';
+    transactions.forEach(addTransactionDOM);
+    updateValues();
+}
+
+init();
+
